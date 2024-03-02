ae781b551adb4e17e1eb6f85522a307f58cde221
udeshika <perera.n.u@gmail.com>
2024-03-02 20:20:05 +0800

    private variables commit

---
+++ senior/private-data/test/Attack.ts
@@ -0,0 +1,25 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Attack to Login Contract", () => {
+  it("Should be able to read private variables", async() => {
+    const userName = ethers.encodeBytes32String("admin");
+    const password = ethers.encodeBytes32String("password");
+
+    const loginContractFactory = await ethers.getContractFactory("Login");
+    const login = await loginContractFactory.deploy(userName, password);
+
+    const slot0Bytes = ethers.provider.getStorage(login.target,0);
+    const slot1Bytes = ethers.provider.getStorage(login.target,1);
+
+    expect(ethers.decodeBytes32String(await slot0Bytes)).to.equal("admin");
+    expect(ethers.decodeBytes32String(await slot1Bytes)).to.equal("password");
+    
+
+  });
+});
