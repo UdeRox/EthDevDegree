f7f12bb090b88dadbd8704381bebb8dd41747e15
udeshika <perera.n.u@gmail.com>
2024-03-02 14:36:07 +0800

    update test cases

---
+++ senior/delegate-call/test/Student.ts
@@ -0,0 +1,14 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("Student Contract delegate-call", () => {
+    it("Should call the deletegate and add function", async () => {
+        const [_, address1] = await ethers.getSigners();
+        const studentFactory = await ethers.getContractFactory("Student");
+        const student = await studentFactory.deploy();
+        const calculatorFactory = await ethers.getContractFactory("Calculator");
+        const calculator = await calculatorFactory.deploy();
+        await student.addTwoNumbers(calculator.getAddress(), 1, 2);
+        expect(await student.mySum()).to.equal(3);
+    })
+})
