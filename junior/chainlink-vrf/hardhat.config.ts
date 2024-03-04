9b47cd4ec411786ca5d756315476f1fbaf1d8533
udeshika <perera.n.u@gmail.com>
2024-03-05 01:10:51 +0800

    updated config file

--- junior/chainlink-vrf/hardhat.config.ts
+++ junior/chainlink-vrf/hardhat.config.ts
@@ -1,8 +1,25 @@
+//@ts-ignore
+// @ts-nocheck
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("dotenv").config({ path: ".env" });
 
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    mumbai: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: POLYGONSCAN_KEY,
+    },
+  },
 };
 
 export default config;
